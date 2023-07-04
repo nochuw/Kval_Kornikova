0b0053db3ea90c6eb58842e4677705cfb332fa60
Анастасия Подколзина <appodkolzina@mail.ru>
2023-07-04 10:24:29 +0300

    Добавьте файлы проекта.

---
+++ Kval(Kornikova)/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Kval_Kornikova_
+{
+    public class College : IComparable<College>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Teacher> Teachers { get; set; }
+
+        public College(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Teachers = new List<Teacher>();
+        }
+        public void AddTeacher(Teacher teacher)
+        {
+            Teachers.Add(teacher);
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Адрес: {Address} \n");
+            Console.WriteLine("Состав преподавателей:");
+            foreach (Teacher teacher in Teachers)
+            {
+                teacher.Show();
+            }
+        }
+        public int CompareTo(College other)
+        {
+            //Реализация метода сравнения колледжей на основе их имени
+            return string.Compare(Name, other.Name, StringComparison.Ordinal);
+        }
+    }
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Course { get; set; }
+
+        public Teacher(string name, string course)
+        {
+            Name = name;
+            Course = course;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("ФИО: " + Name);
+            Console.WriteLine("Дисциплина: " + Course);
+        }
+    }
+}
---
+++ MainProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kval_Kornikova_;
+
+namespace MainProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание объекта основного класса College
+            College college = new College("1Мок", "Староватутинский проезд");
+
+            //Создание объектов вспомогательного класса Teacher
+            Teacher tch1 = new Teacher("Ляпина О.П.", "МиАПО \n");
+            Teacher tch2 = new Teacher("Гусева А.В.", "МиАПО \n");
+            Teacher tch3 = new Teacher("Тузовский А.Ф.", "ТРПО \n");
+            Teacher tch4 = new Teacher("Баринова Т.Г.", "Ин.язык");
+
+            //Добавление созданных объектов Teacher в список Teachers класса College
+            college.Teachers.Add(tch1);
+            college.Teachers.Add(tch2);
+            college.Teachers.Add(tch3);
+            college.Teachers.Add(tch4);
+
+            //Показ содержимого всех объектов
+            college.ShowAll();
+
+            Console.ReadKey();
+        }
+    }
+}
